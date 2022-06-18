04b4c2f8542cb9ceef8fc112dcd46daa62ede090
Ian Schoen <ian.rocha.schoen@gmail.com>
2022-06-18 03:35:26 -0600

    Clean up plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,4 +1,4 @@
--- [[ plug.lua ]]
+-- [[ plugins.lua ]]
 
 local fn = vim.fn
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
@@ -23,13 +23,16 @@ require("packer").init({
 
 return require("packer").startup(function(use)
   -- [[ Package Manager ]]
-  use { "wbthomason/packer.nvim" }                              -- Package manager
 
+  use { "wbthomason/packer.nvim" }                              -- Package manager
+--------------------------------------------------------------------------------------------------------------------------------------
   -- [[ LSP ]]
+
   use { "neovim/nvim-lspconfig" }                               -- LSP defaults
   use { "anott03/nvim-lspinstall" }                             -- Language Server Installer
-
+--------------------------------------------------------------------------------------------------------------------------------------
   -- [[ Auto-completion ]]
+
   use { "saadparwaiz1/cmp_luasnip" }                            -- Generates completion source from LuaSnip
   use { "hrsh7th/cmp-nvim-lsp" }                                -- Generates completion source from LSP
   use { "hrsh7th/cmp-buffer" }                                  -- Generates completion source from buffer
@@ -38,44 +41,49 @@ return require("packer").startup(function(use)
   use { "hrsh7th/nvim-cmp" }                                    -- Consumes all completion sources and provides suggestions
   use { "nvim-lua/completion-nvim" }                            -- Provides autocompletion functionality for nvim
   use { "onsails/lspkind.nvim" }                                -- Provides pictograms to NeoVim LSP suggestions
-
+--------------------------------------------------------------------------------------------------------------------------------------
   -- [[ Fuzzy Finding ]]
+
   use { "nvim-lua/plenary.nvim" }                               -- Useful functions using the NeoVim API
   use { "nvim-lua/popup.nvim" }                                 -- Implementation of the Vim popup API in NeoVim
   use { "nvim-lua/telescope.nvim" }                             -- Extendable fuzzy finder over lists
   use { "jremmen/vim-ripgrep" }                                 -- BLAZINGLY fast line-oriented search tool
-
+--------------------------------------------------------------------------------------------------------------------------------------
   -- [[ Git ]]
+
   use { "airblade/vim-gitgutter" }                              -- Display git diffs in the gutter
   use { "f-person/git-blame.nvim" }                             -- Display Git blame information
-
+--------------------------------------------------------------------------------------------------------------------------------------
   -- [[ UI ]]
+
+  use { "stevearc/dressing.nvim" }                              -- Improve UI using the built-in core UI hooks `vim.ui.select` and `vim.ui.input`
   use {
     "nvim-lualine/lualine.nvim",                                -- Display useful information in status line
     requires = { "kyazdani42/nvim-web-devicons", opt = true }
   }
-  use { "stevearc/dressing.nvim" }                              -- Improve UI using the built-in core UI hooks `vim.ui.select` and `vim.ui.input`
   use {
     "goolord/alpha-nvim",                                       -- Customize Neovim startup screen
-    requires = { "kyazdani42/nvim-web-devicons" },
---    config = function ()
---        require("alpha").setup(require("alpha.themes.startify").config)
---    end
+    requires = { "kyazdani42/nvim-web-devicons" }
   }
-
+--------------------------------------------------------------------------------------------------------------------------------------
   -- [[ Treesitter ]]
+
   use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }  -- Parser generator tool and an incremental parsing library
-
+--------------------------------------------------------------------------------------------------------------------------------------
   -- [[ Formatting ]]
+
   use { "prettier/vim-prettier", run = "yarn install" }         -- Formatter for JavaScript, TypeScript, HTML, CSS, JSON, YAML, Markdown, GraphQL, etc.
-
+--------------------------------------------------------------------------------------------------------------------------------------
   -- [[ Snippets ]]
+
   use { "L3MON4D3/LuaSnip" }                                    -- Snippet engine
-
+--------------------------------------------------------------------------------------------------------------------------------------
   -- [[ Themes ]]
-  use { "TroyFletcher/vim-colors-synthwave" }                   -- Colorscheme - Synthwave
 
+  use { "TroyFletcher/vim-colors-synthwave" }                   -- Colorscheme - Synthwave
+--------------------------------------------------------------------------------------------------------------------------------------
   -- [[ Editing ]]
+
   use { "tpope/vim-surround" }                                  -- Vim nouns, motions, and verbs for changing surrounding characters
   use { "tpope/vim-repeat" }                                    -- Allow `.` to repeat actions performed by plugins
 end)
