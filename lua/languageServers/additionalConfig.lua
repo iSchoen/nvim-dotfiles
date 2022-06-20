7bf19dde9e968ad967a33a4b08512f42886216fa
Ian Schoen <ian.rocha.schoen@gmail.com>
2022-06-20 02:56:46 -0600

    Disable virtual text diag; Update float config

--- lua/languageServers/additionalConfig.lua
+++ lua/languageServers/additionalConfig.lua
@@ -3,7 +3,13 @@
 -- Configure diagnostic window
 vim.diagnostic.config({
   underline = true,
-  virtual_text = true,
+  virtual_text = false,
   signs = true,
   update_in_insert = true,
+  severity_sort = true,
+  float = {
+    scope = "cursor",
+    source = true,
+  },
 })
+
