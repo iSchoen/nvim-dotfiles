40b89b72ab322ec4614eebb1355600439334d3c2
Ian Schoen <ian.rocha.schoen@gmail.com>
2022-06-20 02:59:29 -0600

    Add json lsp server

--- lua/languageServers/init.lua
+++ lua/languageServers/init.lua
@@ -2,6 +2,7 @@
 
 require("languageServers.additionalConfig")
 require("languageServers.eslint")
+require("languageServers.json")
 require("languageServers.lua")
 require("languageServers.ruby")
 require("languageServers.typescript")
---
+++ lua/languageServers/json.lua
@@ -0,0 +1,8 @@
+-- [[ languageServers.json.lua ]]
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+require"lspconfig".jsonls.setup({
+  capabilities = capabilities,
+})
