06b302f7ee0c74e1b1144e2be098cce859b833d1
Ian Schoen <ian.rocha.schoen@gmail.com>
2022-07-01 00:45:05 -0600

    Update ruby.lua

--- lua/languageServers/ruby.lua
+++ lua/languageServers/ruby.lua
@@ -4,7 +4,7 @@
 local lspconfig = require("lspconfig")
 local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-lspconfig.solargraph.setup{
+lspconfig.solargraph.setup({
   capabilities = capabilities
-}
+})
 
