ebda63ca1dceb075ca6ac167df7dec98f46a1d1b
Ian Schoen <ian.rocha.schoen@gmail.com>
2022-07-01 00:44:58 -0600

    Update json.lua

--- lua/languageServers/json.lua
+++ lua/languageServers/json.lua
@@ -1,8 +1,10 @@
 -- [[ languageServers.json.lua ]]
 
+local lspconfig = require("lspconfig")
 local capabilities = vim.lsp.protocol.make_client_capabilities()
+
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-require"lspconfig".jsonls.setup({
+lspconfig.jsonls.setup({
   capabilities = capabilities,
 })
