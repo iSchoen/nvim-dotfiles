eaab82dfb60774b58dc581fe643ec22acdb8ebc7
Ian Schoen <ian.rocha.schoen@gmail.com>
2022-07-01 00:45:01 -0600

    Update lua.lua

--- lua/languageServers/lua.lua
+++ lua/languageServers/lua.lua
@@ -2,7 +2,6 @@
 -- Run `brew install lua-language-server`
 
 local lspconfig = require("lspconfig")
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 lspconfig.sumneko_lua.setup{
   settings = {
