64e38a72ae42b7d3d4d868aa4c10458cda2b2394
Ian Schoen <ian.rocha.schoen@gmail.com>
2022-07-01 00:44:52 -0600

    Update eslint.lua

--- lua/languageServers/eslint.lua
+++ lua/languageServers/eslint.lua
@@ -2,7 +2,6 @@
 -- Run `npm i -g vscode-langservers-extracted`
 
 local lspconfig = require("lspconfig")
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-lspconfig.eslint.setup{}
+lspconfig.eslint.setup({})
 
