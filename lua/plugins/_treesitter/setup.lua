7780db859a8e6b2626887da6302d11ea49ed8fa5
Ian Schoen <ian.rocha.schoen@gmail.com>
2024-06-10 10:11:38 -0600

    Update treesitter ensure_installed languages

--- lua/plugins/_treesitter/setup.lua
+++ lua/plugins/_treesitter/setup.lua
@@ -2,7 +2,18 @@
 
 require("nvim-treesitter.configs").setup({
 	-- Add languages to be installed here that you want installed for treesitter
-	ensure_installed = { "c", "cpp", "go", "lua", "python", "rust", "typescript", "vimdoc" },
+	ensure_installed = {
+		"c",
+		"cpp",
+		"go",
+		"lua",
+		"python",
+		"rust",
+		"javascript",
+		"typescript",
+		"tsx",
+		"vimdoc"
+	},
 
 	highlight = {
 		enable = true
