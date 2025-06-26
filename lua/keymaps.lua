4a659b81018142356b33335488d29e4f524f8a60
iSchoen <ian.rocha.schoen@gmail.com>
2025-06-26 05:13:19 -0600

    Add keymaps for navigating quickfix

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,3 +5,6 @@ vim.keymap.set("n", "<C-k>", "<C-w>k", {})
 vim.keymap.set("n", "<C-l>", "<C-w>l", {})
 
 vim.keymap.set("n", "<leader>/", ":nohlsearch<CR>", { silent = true, noremap = true })
+
+vim.keymap.set("n", "<C-n>", ":cnext<CR>", {})
+vim.keymap.set("n", "<C-p>", ":cprevious<CR>", {})
