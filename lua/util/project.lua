ab8c733bcf9b0da2cbd52330b02a975f2d154c42
iSchoen <ian.rocha.schoen@gmail.com>
2025-06-29 19:25:07 -0600

    Fix logic in has_biome

--- lua/util/project.lua
+++ lua/util/project.lua
@@ -17,13 +17,16 @@ end
 
 function M.has_biome()
 	local bufname = vim.api.nvim_buf_get_name(0)
-	local root = vim.fs.dirname(
-		vim.fs.find({ "package.json", "deno.json", "deno.jsonc", ".git" }, { upward = true, path = bufname })[1]
-			or bufname
+	local found_files = vim.fs.find(
+		{ "biome.json", "biome.jsonc", "deno.json", "deno.jsonc", ".git" },
+		{ upward = true, path = bufname }
 	)
 
+	local root = found_files[1] and vim.fs.dirname(found_files[1]) or vim.fs.dirname(bufname)
+
 	local function exists(name)
-		return uv.fs_stat(root .. "/" .. name) ~= nil
+		local stat = uv.fs_stat(root .. "/" .. name)
+		return stat ~= nil
 	end
 
 	return exists("biome.json") or exists("biome.jsonc")
