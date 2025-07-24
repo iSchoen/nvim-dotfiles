8029bd63ce498a92aa2d011a0c72dfe9cd7d40ef
iSchoen <ian.rocha.schoen@gmail.com>
2025-07-24 09:38:24 -0600

    Improve bracket autopairing with Blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -16,11 +16,13 @@ return {
 		},
 
 		appearance = {
-			nerd_font_variant = "mono"
+			nerd_font_variant = "mono",
 		},
 
 		completion = {
 			accept = { auto_brackets = { enabled = false } },
+
+			ghost_text = { enabled = false },
 
 			menu = {
 				draw = {
@@ -54,7 +56,40 @@ return {
 			default = { "lsp", "path", "snippets", "buffer" },
 		},
 
-		fuzzy = { implementation = "prefer_rust_with_warning" }
+		snippets = {
+			expand = function(snippet)
+				-- Smart snippet handling: remove all placeholders, keep smart positioning
+				local text = snippet
+
+				-- Remove all placeholders (both with and without default values)
+				text = text:gsub("${%d+:[^}]*}", "") -- Remove ${1:default}
+				text = text:gsub("${%d+}", "") -- Remove ${1}
+				text = text:gsub("$%d+", "") -- Remove $1
+
+				-- Insert the text
+				local lines = vim.split(text, "\n")
+				vim.api.nvim_put(lines, "c", true, true)
+
+				-- Smart cursor positioning for arrays and objects
+				local line = vim.api.nvim_get_current_line()
+				local col = vim.api.nvim_win_get_cursor(0)[2]
+
+				-- If we just inserted [] or {}, move cursor inside
+				if line:find("%[%]", col - 1) then
+					vim.api.nvim_win_set_cursor(0, { vim.api.nvim_win_get_cursor(0)[1], col - 1 })
+				elseif line:find("%{%}", col - 1) then
+					vim.api.nvim_win_set_cursor(0, { vim.api.nvim_win_get_cursor(0)[1], col - 1 })
+				end
+			end,
+			active = function()
+				return false
+			end,
+			jump = function()
+				return false
+			end,
+		},
+
+		fuzzy = { implementation = "prefer_rust_with_warning" },
 	},
-	opts_extend = { "sources.default" }
+	opts_extend = { "sources.default" },
 }
