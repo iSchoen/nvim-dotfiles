6dfdfbdf6e8161ba980beb49b8cbf1dd3501e4bc
iSchoen <ian.rocha.schoen@gmail.com>
2025-10-31 21:44:21 -0600

    Update gitsigns.lua

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -18,5 +18,14 @@ return {
 				text = "~",
 			},
 		},
+		current_line_blame = true,
+		current_line_blame_opts = {
+			virt_text = true,
+			virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+			delay = 500,
+			ignore_whitespace = false,
+			virt_text_priority = 100,
+			use_focus = true,
+		},
 	},
 }
