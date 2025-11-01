198b15c9b3b18c8c7335bc773bd2c46df510dbb2
iSchoen <ian.rocha.schoen@gmail.com>
2025-10-31 21:44:12 -0600

    Add commented fzf-lua

---
+++ lua/plugins/fzf-lua.lua
@@ -0,0 +1,22 @@
+-- brew install fzf
+
+return {
+	-- "ibhagwan/fzf-lua",
+	-- dependencies = { "nvim-tree/nvim-web-devicons" },
+	--
+	-- config = function()
+	-- 	local fzf_lua = require("fzf-lua")
+	--
+	-- 	fzf_lua.setup({
+	-- 		"ivy",
+	-- 		keymap = {
+	-- 			fzf = {
+	-- 				["ctrl-q"] = "select-all+accept",
+	-- 			},
+	-- 		},
+	-- 	})
+	--
+	-- 	vim.keymap.set("n", "<leader>t", fzf_lua.files, {})
+	-- 	vim.keymap.set("n", "<leader>gs", fzf_lua.live_grep, {})
+	-- end,
+}
